f90804113e6397a25d107860593ce17d75cd3a2b
Mortvola <mortvola@gmail.com>
2023-11-13 06:11:25 -0700

    Used eject so that webpack config can be modified to import wgsl files. Initial changes to make wgsl files importable.

--- src/Pipeline.ts
+++ src/Pipeline.ts
@@ -1,37 +1,7 @@
 import { bindGroups } from "./BindGroups";
 import { gpu } from "./Gpu";
 import Mesh from "./Mesh";
-
-const shaders = `
-struct Vertex {
-  @location(0) position: vec4f,
-  @location(1) color: vec4f,
-}
-
-struct VertexOut {
-  @builtin(position) position : vec4f,
-  @location(0) color : vec4f
-}
-
-@group(0) @binding(0) var<uniform> perspective: mat4x4f;
-@group(1) @binding(0) var<uniform> model: mat4x4f;
-
-@vertex
-fn vertex_main(vert: Vertex) -> VertexOut
-{
-  var output : VertexOut;
-
-  output.position = perspective * model * vert.position;
-  output.color = vert.color;
-  return output;
-}
-
-@fragment
-fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
-{
-  return fragData.color;
-}
-`;
+import simpleShader from './shaders/simple.wgsl';
 
 const vertexBufferLayout: GPUVertexBufferLayout[] = [
   {
@@ -67,23 +37,8 @@ class Pipeline {
     this.device = gpu.device;
 
     const shaderModule = gpu.device.createShaderModule({
-      code: shaders,
+      code: simpleShader,
     })
-    
-    // const bindGroupLayout = device.createBindGroupLayout({
-    //   entries: [
-    //     {
-    //       binding: 0,
-    //       visibility: GPUShaderStage.VERTEX,
-    //       buffer: {},
-    //     },
-    //     {
-    //       binding: 1,
-    //       visibility: GPUShaderStage.VERTEX,
-    //       buffer: {},
-    //     }
-    //   ]
-    // })
     
     const pipelineLayout = gpu.device.createPipelineLayout({
       bindGroupLayouts: bindGroups.layouts(),
